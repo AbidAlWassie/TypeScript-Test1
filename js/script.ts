b60e6df7f19be49a393999525510af98508acae0
Abid Al-Wasi <abidalwasi@outlook.com>
2021-06-21 19:28:51 +0600

    updated project

--- js/script.ts
+++ js/script.ts
@@ -1,9 +1,9 @@
 const button = document.getElementById("button");
-const input1 = document.getElementById("num1");
-const input2 = document.getElementById("num2");
+const input1 = document.getElementById("num1")! as HTMLInputElement;
+const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-function add(num1, num2) {
-  return num1 + num2;
+function add(num1:number, num2:number) {
+  return num1 + num2; 
 }
 
 button.addEventListener("click", function() {
